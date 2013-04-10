380c022b183b6585b38b42f107aee2774dd1f84c
Joachim Nolten <joachim@nolten.org>
2013-04-10 10:22:22 +0200

    Let's rethink the rescue-all situation

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'fog', :git => "git@github.com:stiller/fog.git", :branch => 'path_style'
 gem 'pry'
-gem 'pry-debugger'
+#gem 'pry-debugger'
 gem 'parallel'
+gem 'thread_safe'
--- s3_to_ceph.rb
+++ s3_to_ceph.rb
@@ -15,13 +15,16 @@
 #ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #DEALINGS IN THE SOFTWARE.
 
+require 'rubygems'
+require 'bundler/setup'
+require 'thread_safe'
 require 'logger'
 require 'yaml'
 require 'fog'
 require 'pry'
 require 'tempfile'
 require 'parallel'
-
+require 'timeout'
 # No longer necesarry for next Fog release
 module Excon
   class Connection
@@ -32,6 +35,16 @@ end
 Excon.defaults[:ssl_verify_peer] = false
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+module MIME
+  class Types
+    def initialize(data_version = nil)
+      @type_variants    = ThreadSafe::Hash.new { |h, k| h[k] = [] }
+      @extension_index  = ThreadSafe::Hash.new { |h, k| h[k] = [] }
+      @data_version = data_version
+    end
+  end
+end
 
 module S3Backup
 
@@ -71,12 +84,12 @@ module S3Backup
   files = s3.directories.get('images.eu.viewbook.com').files
 
 
-  subset = files.all(:marker => '398129e09d7ed7c5004661e7da9aff58.jpg')
-  subset.each_file_this_page
+  subset = files.all(:marker => '56c33849662fab07a58bb76a5f083bfa.jpg')
 
   def S3Backup.parallel_copy files, target_dir, logger
-    Parallel.each(files, :in_threads => 128) do |s3_file|
-      #unless try_this(3, "Ceph HEAD error") { @target_dir.files.head(s3_file.key) }
+    Parallel.each(files, :in_threads => 12) do |s3_file|
+    begin
+    Timeout.timeout(60) do
       logger.info(s3_file.key)
       tempfile = Tempfile.new(s3_file.key)
       try_this(3, "S3 error") do
@@ -85,8 +98,12 @@ module S3Backup
       try_this(3, "Ceph error") do
         target_dir.files.create(:key => s3_file.key, :body => tempfile, :public => true )
       end
+      tempfile.close
       tempfile.unlink
-      #end
+    end
+    rescue Timeout::Error
+        $stderr.puts("Timeout")
+    end
     end
   end
 
@@ -106,6 +123,7 @@ module S3Backup
 
   parallel_copy subset, ceph_dir, mylogger
   while subset.is_truncated
+    s3.sync_clock
     subset = subset.all(:marker => subset.last.key)
     parallel_copy subset, ceph_dir, mylogger
   end
